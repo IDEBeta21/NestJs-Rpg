032c82f5708a9d1fef78d5e56f3678bc93a9ba82
Ian Deladia (Ermino) <kitsmype416@gmail.com>
2025-01-12 18:58:28 +0800

    Remove https cert (for now)

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as fs from 'fs';
 
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./src/cert/key.pem'),
-    cert: fs.readFileSync('./src/cert/cert.pem'),
-  };
-
   const app = await NestFactory.create(
     AppModule,
-    { httpsOptions },
   );
   app.useGlobalPipes(
     new ValidationPipe()
@@ -31,9 +24,9 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 
   if(process.env.NODE_ENV == 'dev'){
-    console.log("View Swagger in: https://localhost:3001/swagger")
+    console.log("View Swagger in: http://localhost:3001/swagger")
   }else{
-    console.log("View Swagger in: https://localhost:3000/swagger")
+    console.log("View Swagger in: http://localhost:3000/swagger")
   }
 }
 bootstrap();
